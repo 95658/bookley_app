3d4f070c4a227bae01153d7481f7e68370952950
Ahmed Abdel-Fatah <aabdelfatah732@gmail.com>
2023-08-23 21:23:23 +0300

    featured books cubit & states complete

--- lib/features/Home/presentation/views/management/FeaturedBooksCubit/featured_books_cubit.dart
+++ lib/features/Home/presentation/views/management/FeaturedBooksCubit/featured_books_cubit.dart
@@ -1,10 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookley_app/features/Home/data/models/BookModel/book_model.dart';
+import 'package:bookley_app/features/Home/data/models/repose/home_repo.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/cupertino.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void > fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.errMessage));
+    }, (books){
+      emit(FeaturedBooksSuccess(books.cast<BookModel>()));
+    });
+  }
 }
