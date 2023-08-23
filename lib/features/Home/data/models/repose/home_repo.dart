5563c830fb2747bfcf4161dcf55465cced5143d8
Ahmed Abdel-Fatah <aabdelfatah732@gmail.com>
2023-08-23 17:16:47 +0300

    book model and home repo

--- lib/features/Home/data/models/repose/home_repo.dart
+++ lib/features/Home/data/models/repose/home_repo.dart
@@ -1,4 +1,6 @@
+import 'package:bookley_app/features/Home/data/models/BookModel/Book_model.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<List<BookModel>> fetchBestSellerBooks();
+  Future<List<BookModel>> fetchFeaturedBooks();
 }
